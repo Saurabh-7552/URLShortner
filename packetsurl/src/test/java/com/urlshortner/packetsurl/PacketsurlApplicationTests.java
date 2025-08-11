465690064a7406079f3cfbadb906eede502e5521
Saurabh Kumar <sk0497552@gmail.com>
2025-08-11 13:16:03 +0530

    intialize the project

---
+++ packetsurl/src/test/java/com/urlshortner/packetsurl/PacketsurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortner.packetsurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketsurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
